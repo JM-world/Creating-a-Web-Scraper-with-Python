b5319b4d6762d7f7bbf16248909605287608d603
Jongmoon Jeong <unhoiylaw@naver.com>
2023-08-17 15:32:08 +0900

    Py

--- 02.and_or.py
+++ 02.and_or.py
@@ -4,7 +4,7 @@ if age < 18:
     print("You can't drink.")
 elif age >= 18 and age <= 35:
     print("You drink beer!")
-elif age == 60 or age ==70:
+elif age == 60 or age ==80:
     print("Birthday party!")
 else:
     print("Go ahead!")
