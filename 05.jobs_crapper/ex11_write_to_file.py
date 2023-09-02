1eac6785e91d73128e22028e8fcfd8c4954fe153
linxiangkaiho <unhoiylaw@naver.com>
2023-09-02 23:35:55 +0900

    study

--- 05.jobs_crapper/ex11_write_to_file.py
+++ 05.jobs_crapper/ex11_write_to_file.py
@@ -10,7 +10,7 @@ jobs = indeed + wwr
 
 for job in jobs:
     print(job)
-    print("/////\n/////")
+    print("//////\n//////")
 
 file = open(f"{keyword}.scv", "w")   #UnicodeEncodeError 'cp949' codec can't encode character 오류 날 시, file = open(f"{keyword}.csv", "w", encoding="utf-8")로 수정하기.
                                      # 이후 엑셀에서도 깨질 떈, file = open(f"{keyword}.csv", "w", encoding="utf-8-sig")로 변경.
